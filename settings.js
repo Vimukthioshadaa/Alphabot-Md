c35355f2a2720f9ed8f5a21f620fd6ac8e87a6c4
VIMUKTHI OSHADA <96766297+Kavee2476@users.noreply.github.com>
2022-02-03 00:41:30 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ"
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.myweb ="https://api-alphabot.herokuapp.com/"
-global.youtube = "https://youtube.com/c/ZeeoneOfc"
-global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "62887435047326"
-global.ownernomerr = "+62887435047326"
+global.botname = "𝑀𝑟ツ𝑉𝑖𝑚𝑢𝑤𝑎"
+global.ownername= "𝑀𝑅ツ𝑉𝑖𝑚𝑢𝑤𝑎"
+global.myweb ="https://youtube.com/channel/UCZoiMxv1XWRKk4qT-dZ9uqA"
+global.youtube = "https://youtube.com/channel/UCZoiMxv1XWRKk4qT-dZ9uqA"
+global.github = "https://github.com/"
+global.ownernomer = "94775792013"
+global.ownernomerr = "+94775792013"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm
+global.owner = ["94775792013","94775792013","94775792013"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© 𝑀𝑅ツ𝑉𝑖𝑚𝑢𝑤𝑎' //sticker wm
+global.author = 'Di Buat Oleh Vimuwa' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
